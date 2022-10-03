2b5373bceb75b12ca18444ab48378c832cdce72c
dake-kadyrbek <77795358+dake-kadyrbek@users.noreply.github.com>
2022-10-03 19:25:27 +0600

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x68DDFF3FD445f00D6Db5BF61D281Bb9af519a812";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
